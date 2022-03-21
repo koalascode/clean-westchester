82f1f66b94d6ffe383675497468e6d7f3f2e14db
koalascode <90786950+koalascode@users.noreply.github.com>
2022-03-21 16:00:07 -0400

    potential fix for mobile spacing for nav

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6e96276e05e63607"; }
+/******/ 		__webpack_require__.h = function() { return "f9698997985eba16"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
