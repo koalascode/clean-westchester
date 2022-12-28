2363754cbd04a0203f84c4c0feecb4a0c13ce172
Aaron Anidjar <anidjaraaron@gmail.com>
2022-12-28 08:50:18 -0700

    unoptimized imgs

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ module.exports = {
   images: {
       domains: ['lh3.googleusercontent.com', 'lh4.googleusercontent.com', 'lh5.googleusercontent.com', 'lh6.googleusercontent.com', 's3.us-west-2.amazonaws.com', 'cleanwestchester.s3.amazonaws.com'],
   },
-  experimental: { images: { allowFutureImage: true } }
+  experimental: { images: { allowFutureImage: true } },
+  images: {
+    unoptimized: true,
+  },
 }
 
